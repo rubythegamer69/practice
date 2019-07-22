62ab2df485068be6c8db0987d91d4f63baf1d853
rubythegamer69 <51950827+rubythegamer69@users.noreply.github.com>
2019-07-22 11:32:12 +0300

    Update triangles.c

--- triangles.c
+++ triangles.c
@@ -5,19 +5,19 @@ int main(void)
     int a, b, c;
     printf("Enter the base of the triangle\n");            //input of triangle sides
     scanf("%d", &a);
-    do
+    do                                                    //do while loop to make sure a right angled triangle is not mistaken for a scalene
     {
         printf("Enter the other two sides of the triangle,starting with the smallest side\n");
         printf("side 1\n");
         scanf("%d", &b);
         printf("side 2\n");
         scanf("%d", &c); 
-        if (b>c)
+        if ( b > c)
         {
             printf("Please check your input.\nYou input %d first which is greater that %d\nPlease start with the smaller side.\n", b, c);
         }
-    }
-    while(b>c);
+    } 
+    while(b > c);
     
     
     if ((c * c) == ((a * a) + (b * b)))                    //check if triangle is right angled
